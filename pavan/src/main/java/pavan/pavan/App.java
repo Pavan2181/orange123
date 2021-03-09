e435690285e15a8581f4062894cd046ae1a7f47b
Pavan Kumar <Pavan Kumar@MSI>
2021-03-09 15:40:35 +0530

    pavan

---
+++ pavan/src/main/java/pavan/pavan/App.java
@@ -0,0 +1,13 @@
+package pavan.pavan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
